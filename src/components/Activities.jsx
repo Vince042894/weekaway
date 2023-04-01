0346e9bc1c7e1045b6b9d8dbe90cf7f101cee9a7
runel042894 <85103751+runel042894@users.noreply.github.com>
2023-04-01 22:28:23 +0800

    Change imge size

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -8,7 +8,7 @@ const Activities = () => {
           Lorem
         </h3>
         <img
-          className="w-full h-96 object-cover relative border-4 border-white shadow-lg "
+          className="w-full h-60 object-cover relative border-4 border-white shadow-lg "
           src="https://images.pexels.com/photos/1472887/pexels-photo-1472887.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="/"
         />
@@ -18,7 +18,7 @@ const Activities = () => {
           Lorem
         </h3>
         <img
-          className="w-full h-96 object-cover relative border-4 border-white shadow-lg"
+          className="w-full h-60 object-cover relative border-4 border-white shadow-lg"
           src="https://images.unsplash.com/photo-1586508577428-120d6b072945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGRpdmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=600"
           alt="/"
         />
@@ -28,7 +28,7 @@ const Activities = () => {
           Lorem
         </h3>
         <img
-          className="w-full h-96 object-cover relative border-4 border-white shadow-lg"
+          className="w-full h-60 object-cover relative border-4 border-white shadow-lg"
           src="https://images.unsplash.com/photo-1440130266107-787dd24d69d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhcmFnbGlkZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=600"
           alt="/"
         />
